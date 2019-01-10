dfda365cf462cab5ebc5538327155b2b504f5f37
Milena <milena.bd@gmail.com>
2019-01-10 12:40:26 +0200

    Initial

---
+++ src/main/java/com/smdev/timetracking/ttbackend/TtBackendApplication.java
@@ -0,0 +1,14 @@
+package com.smdev.timetracking.ttbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtBackendApplication.class, args);
+	}
+
+}
+
